13e99a52b3d661a851ff6a94677110cfa58a2b23
Tomonori <gutrse3321@live.com>
2019-05-28 20:45:42 +0800

    .fix:clear cookie

--- define/cookie.go
+++ define/cookie.go
@@ -13,37 +13,7 @@ const (
 func GetCookies() []*http.Cookie {
 	cookies := make([]*http.Cookie, 4)
 
-	cookies[0] = &http.Cookie{
-		Name: "igneous",
-		Value: "45e5748db",
-		Path: "/",
-		Domain: ".exhentai.org",
-		MaxAge: maxAge,
-	}
 
-	cookies[1] = &http.Cookie{
-		Name: "ipb_member_id",
-		Value: "1044069",
-		Path: "/",
-		Domain: ".exhentai.org",
-		MaxAge: maxAge,
-	}
-
-	cookies[2] = &http.Cookie{
-		Name: "ipb_pass_hash",
-		Value: "4268d5c05c94cdedf124eabf2f1e7c95",
-		Path: "/",
-		Domain: ".exhentai.org",
-		MaxAge: maxAge,
-	}
-
-	cookies[3] = &http.Cookie{
-		Name: "sk",
-		Value: "3hl0ggzrgfvcsp3wdu4tarft1k7v",
-		Path: "/",
-		Domain: ".exhentai.org",
-		MaxAge: maxAge,
-	}
 
 	return cookies
 }
--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 		r.Headers.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7")
 		r.Headers.Set("Cache-Control", "max-age=0")
 		r.Headers.Set("Connection", "keep-alive")
-		r.Headers.Set("Cookie", "ipb_member_id=1044069; ipb_pass_hash=4268d5c05c94cdedf124eabf2f1e7c95; igneous=45e5748db; sk=3hl0ggzrgfvcsp3wdu4tarft1k7v")
+		r.Headers.Set("Cookie", "")
 		fmt.Println("Visiting", r.URL.String())
 	})
 
